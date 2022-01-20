1f7105e69b7b7f3505e4317895d6ec27fc10646c
Wei Ding <ding_web@yahoo.co.jp>
2022-01-19 19:45:25 -0800

    Initial commit

---
+++ Ch01/HelloNet5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloNet5
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello Net 5.0!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CS10Net6VS2022
+ Examine C# 10, Net 6.0 and VS 2022
