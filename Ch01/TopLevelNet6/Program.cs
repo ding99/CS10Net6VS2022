ddbb6356bcc474317ec39d3f93b257fd55d70d53
Wei Ding <ding_web@yahoo.co.jp>
2022-01-19 20:20:31 -0800

    create top-level net6

---
+++ Ch01/TopLevelNet6/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.BackgroundColor = ConsoleColor.Gray;
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine ("Hello, Net 6.0 top-level program!");
+Console.ResetColor ();
