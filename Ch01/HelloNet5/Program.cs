4283840c91a5657c550060fd34410f19cfbe13b2
Wei Ding <ding_web@yahoo.co.jp>
2022-01-19 19:55:33 -0800

    try to modify

--- Ch01/HelloNet5/Program.cs
+++ Ch01/HelloNet5/Program.cs
@@ -6,7 +6,12 @@ namespace HelloNet5
     {
         static void Main (string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
             Console.WriteLine ("Hello Net 5.0!");
+
+            Console.ResetColor ();
         }
     }
 }
