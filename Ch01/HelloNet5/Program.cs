e21870c26c4460f2b79c74aa7433d523a34bfe9f
Wei Ding <ding_web@yahoo.co.jp>
2022-01-19 20:02:45 -0800

    remove argument

--- Ch01/HelloNet5/Program.cs
+++ Ch01/HelloNet5/Program.cs
@@ -4,12 +4,12 @@ namespace HelloNet5
 {
     internal class Program
     {
-        static void Main (string[] args)
+        static void Main ()
         {
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             Console.ForegroundColor = ConsoleColor.Yellow;
 
-            Console.WriteLine ("Hello Net 5.0!");
+            Console.WriteLine ("Hello, Net 5.0!");
 
             Console.ResetColor ();
         }
