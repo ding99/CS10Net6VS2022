1ed4f1218b70700299e39e8f7e498af742802997
Wei Ding <ding_web@yahoo.co.jp>
2022-10-28 09:55:07 -0700

    sizeof

--- Ch02Grammar/Vocabulary/Program.cs
+++ Ch02Grammar/Vocabulary/Program.cs
@@ -63,6 +63,12 @@ int hexadecimalNotation = 0x_001E_8480;
 Console.WriteLine($"decimalNotion eqauls to binaryNotion: {decimalNotation == binaryNotation}");
 Console.WriteLine($"decimalNotion eqauls to hexadecimalNotation: {decimalNotation == hexadecimalNotation}");
 
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}");
+Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}");
+Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}");
+
 Console.ResetColor();
 Console.WriteLine("== End Chapter 2.");
 
