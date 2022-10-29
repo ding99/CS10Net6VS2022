067cda7120c5776e4b73f08b0b0d1f1db2ed6cbf
Wei Ding <ding_web@yahoo.co.jp>
2022-10-29 12:08:02 -0700

    formatting

---
+++ Ch02Grammar/Formatting/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("- Numbered positional arguments");
+
+int number = 12;
+decimal price = 0.35M;
+Console.WriteLine(
+    format:"{0} apples costs {1:C}",
+    arg0:number,
+    arg1:price * number);
+string formatted = string.Format(
+    format: "{0} apples costs {1:C}",
+    arg0: number,
+    arg1: price * number);
+Console.WriteLine(formatted);
+
+Console.ResetColor();
