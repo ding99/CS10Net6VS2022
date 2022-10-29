4a0457e24d79ee97b5c7d4339024956fb08475cb
Wei Ding <ding_web@yahoo.co.jp>
2022-10-29 10:40:03 -0700

    dynamic type

--- Ch02Grammar/Variables/Program.cs
+++ Ch02Grammar/Variables/Program.cs
@@ -14,6 +14,20 @@ Console.ForegroundColor = ConsoleColor.Cyan;
 Console.WriteLine("- Using dynamic type");
 
 dynamic something = "Ahmed";
-Console.WriteLine($"something type is {something.GetType()}");
+Console.WriteLine($"- something (\"Ahemd\") type is {something.GetType()}");
 Console.WriteLine($"Length is {something.Length}");
+
+something = 12;
+Console.WriteLine($"- something (12) type is {something.GetType()}");
+try {
+    Console.WriteLine($"Length is {something.Length}");
+}
+catch(Exception e) {
+    Console.WriteLine(e.Message);
+}
+
+something = new[] { 3, 5, 7 };
+Console.WriteLine($"- something (new[] {{3,5,7}}) type is {something.GetType()}");
+Console.WriteLine($"Length is {something.Length}");
+
 
