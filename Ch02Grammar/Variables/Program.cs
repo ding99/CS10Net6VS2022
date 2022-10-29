9b56aa6716bcd4ef753ffa18f3055085c7bea33e
Wei Ding <ding_web@yahoo.co.jp>
2022-10-29 10:33:39 -0700

    object type

---
+++ Ch02Grammar/Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("- Using object type");
+
+object height = 1.88;
+object name = "Amir";
+Console.WriteLine($"name type is {name.GetType()}");
+Console.WriteLine($"{name} is {height} metres tall.");
+
+int length2 = ((string)name).Length;
+Console.WriteLine($"{name} has {length2} characters.");
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("- Using dynamic type");
+
+dynamic something = "Ahmed";
+Console.WriteLine($"something type is {something.GetType()}");
+Console.WriteLine($"Length is {something.Length}");
+
