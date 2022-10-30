810fcbc400f8f243aa2c2d1d5254f36cd7bcf565
Wei Ding <ding_web@yahoo.co.jp>
2022-10-30 00:34:28 -0700

    import static class

--- Ch02Grammar/Formatting/Program.cs
+++ Ch02Grammar/Formatting/Program.cs
@@ -1,9 +1,11 @@
-﻿Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine("- Numbered positional arguments");
+﻿using static System.Console;
+
+ForegroundColor = ConsoleColor.Yellow;
+WriteLine("- Numbered positional arguments");
 
 int number = 12;
 decimal price = 0.35M;
-Console.WriteLine(
+WriteLine(
     format:"{0} apples costs {1:C}",
     arg0:number,
     arg1:price * number);
@@ -11,18 +13,18 @@ string formatted = string.Format(
     format: "{0} apples costs {1:C}",
     arg0: number,
     arg1: price * number);
-Console.WriteLine(formatted);
-Console.WriteLine($"{number} apples costs {price * number:C}");
+WriteLine(formatted);
+WriteLine($"{number} apples costs {price * number:C}");
 
-Console.ForegroundColor = ConsoleColor.Cyan;
-Console.WriteLine("- Format strings");
+ForegroundColor = ConsoleColor.Cyan;
+WriteLine("- Format strings");
 
 string applesText = "Apples";
 int applesCount = 1234;
 string bananasText = "Bananas";
 int bananasCount = 56789;
-Console.WriteLine(format:"{0,-10} {1,6:N0}", arg0:"Name", arg1:"Count");
-Console.WriteLine(format: "{0,-10} {1,6:N0}", arg0: applesText, arg1: applesCount);
-Console.WriteLine(format: "{0,-10} {1,6:N0}", arg0: bananasText, arg1: bananasCount);
+WriteLine(format:"{0,-10} {1,6:N0}", arg0:"Name", arg1:"Count");
+WriteLine(format: "{0,-10} {1,6:N0}", arg0: applesText, arg1: applesCount);
+WriteLine(format: "{0,-10} {1,6:N0}", arg0: bananasText, arg1: bananasCount);
 
-Console.ResetColor();
+ResetColor();
