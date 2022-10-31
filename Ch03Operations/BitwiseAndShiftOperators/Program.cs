388ba8b5750ee92c0b4b1c3dc2357ead61ac86e7
Wei Ding <ding_web@yahoo.co.jp>
2022-10-31 09:33:38 -0700

    bitwise operators

---
+++ Ch03Operations/BitwiseAndShiftOperators/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 10;
+int b = 6;
+
+Console.WriteLine($"a = {a} / {ToBinaryString(a)}");
+Console.WriteLine($"a << 3 = {a << 3} / {ToBinaryString(a << 3)}");
+
+static string ToBinaryString(int value) {
+    return Convert.ToString(value, toBase:2).PadLeft(8, '0');
+}
