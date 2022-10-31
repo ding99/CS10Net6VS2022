7d3dd7fa559f95c3fde6413187caf8961d823096
Wei Ding <ding_web@yahoo.co.jp>
2022-10-31 01:20:07 -0700

    real number divisions

--- Ch03Operations/Operators/Program.cs
+++ Ch03Operations/Operators/Program.cs
@@ -1,8 +1,16 @@
 ﻿using static System.Console;
 
-Console.ForegroundColor = ConsoleColor.Green;
+ForegroundColor = ConsoleColor.Green;
 WriteLine("Unary Oprators");
 int a = 3;
 int b = a++;
 int c = ++a;
 WriteLine($"a is {a}, b is {b}, c is {c}");
+
+ForegroundColor = ConsoleColor.Yellow;
+double g = 11.0;
+int f = 3;
+WriteLine($"g is {g:N1}, f is {f}");
+WriteLine($"g /f = {g / f}");
+
+ResetColor();
