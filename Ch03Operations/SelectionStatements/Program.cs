062a26f703048a1bd5236b368209b9e2349b09fd
Wei Ding <ding_web@yahoo.co.jp>
2022-11-01 00:21:25 -0700

    selection

---
+++ Ch03Operations/SelectionStatements/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.ForegroundColor = ConsoleColor.Yellow;
+
+object o = "3";
+int j = 4;
+if (o is int i) {
+    Console.WriteLine($"{i} x {j} = {i * j}");
+}
+else {
+    Console.WriteLine("nothing");
+}
+
+Console.ResetColor();
