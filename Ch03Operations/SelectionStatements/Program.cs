6ace30b280c7de45e22446991fa9ebb2621cd81c
Wei Ding <ding_web@yahoo.co.jp>
2022-11-01 00:34:39 -0700

    switch

--- Ch03Operations/SelectionStatements/Program.cs
+++ Ch03Operations/SelectionStatements/Program.cs
@@ -20,4 +20,30 @@ else {
     Console.WriteLine("o2 is not an int so it cannot multiply!");
 }
 
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("- switch");
+
+int n = (new Random()).Next(1, 7);
+Console.WriteLine($"My random number is {n}");
+switch (n) {
+case 1:
+    Console.WriteLine("One");
+    break;
+case 2:
+    Console.WriteLine("Two");
+    goto case 1;
+case 3:
+case 4:
+    Console.WriteLine("Three or Four");
+    goto case 1;
+case 5:
+    goto A_label;
+default:
+    Console.WriteLine("Default");
+    break;
+}
+Console.WriteLine("After end of switch");
+A_label:
+Console.WriteLine("After A_label");
+
 Console.ResetColor();
