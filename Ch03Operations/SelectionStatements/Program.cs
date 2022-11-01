7d84cf0df8c2731238b381b936a81b4d766e0dba
Wei Ding <ding_web@yahoo.co.jp>
2022-11-01 00:25:55 -0700

    if

--- Ch03Operations/SelectionStatements/Program.cs
+++ Ch03Operations/SelectionStatements/Program.cs
@@ -1,12 +1,23 @@
-﻿Console.ForegroundColor = ConsoleColor.Yellow;
+﻿int j = 4;
 
-object o = "3";
-int j = 4;
-if (o is int i) {
-    Console.WriteLine($"{i} x {j} = {i * j}");
+Console.ForegroundColor = ConsoleColor.Yellow;
+object o1 = "3";
+if (o1 is int i1) {
+    Console.WriteLine("o1 is an int so it can multiply!");
+    Console.WriteLine($"{i1} x {j} = {i1 * j}");
 }
 else {
-    Console.WriteLine("nothing");
+    Console.WriteLine("o1 is not an int so it cannot multiply!");
+}
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+object o2 = 3;
+if (o2 is int i2) {
+    Console.WriteLine("o2 is an int so it can multiply:");
+    Console.WriteLine($"{i2} x {j} = {i2 * j}");
+}
+else {
+    Console.WriteLine("o2 is not an int so it cannot multiply!");
 }
 
 Console.ResetColor();
