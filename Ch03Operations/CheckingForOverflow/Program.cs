031c97ff1f61df71d5cf2e48d3519eaf7c2c0e51
Wei Ding <ding_web@yahoo.co.jp>
2022-11-02 15:04:49 -0700

    check

---
+++ Ch03Operations/CheckingForOverflow/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("- Check for overflow");
+
+checked {
+    int x = int.MaxValue - 1;
+    Console.WriteLine($"Initial value: {x}");
+    x++;
+    Console.WriteLine($"After incrementing: {x}");
+    x++;
+    Console.WriteLine($"After incrementing: {x}");
+    x++;
+    Console.WriteLine($"After incrementing: {x}");
+}
+
+Console.ResetColor();
