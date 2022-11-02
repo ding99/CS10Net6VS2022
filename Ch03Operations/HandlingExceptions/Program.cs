cfbfe7806768d3d3783a71d13ee9381ed51b3fe9
Wei Ding <ding_web@yahoo.co.jp>
2022-11-02 14:12:59 -0700

    specific exceptions

--- Ch03Operations/HandlingExceptions/Program.cs
+++ Ch03Operations/HandlingExceptions/Program.cs
@@ -10,7 +10,27 @@ try {
     int age = int.Parse(input);
     Console.WriteLine($"You are {age} years old.");
 }
+catch (FormatException) {
+    Console.WriteLine("The age you entered is not a valid number format");
+}
 catch(Exception ex) {
+    Console.WriteLine($"{ex.GetType()} says {ex.Message}");
+}
+Console.WriteLine();
+
+input = "9876543210";
+Console.WriteLine($"input is {input}");
+try {
+    int age = int.Parse(input);
+    Console.WriteLine($"You are {age} years old.");
+}
+catch (OverflowException) {
+    Console.WriteLine("Your age is a valid number format it is either too big or small");
+}
+catch (FormatException) {
+    Console.WriteLine("The age you entered is not a valid number format");
+}
+catch (Exception ex) {
     Console.WriteLine($"{ex.GetType()} says {ex.Message}");
 }
 Console.WriteLine("After parsing");
