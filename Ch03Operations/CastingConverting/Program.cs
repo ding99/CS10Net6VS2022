2c1ed86b7ae023c4e577e6f75def6f90e7b3eedc
Wei Ding <ding_web@yahoo.co.jp>
2022-11-02 00:29:09 -0700

    cast

---
+++ Ch03Operations/CastingConverting/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("- Casting Converting");
+
+int a = 10;
+double b = a;
+Console.WriteLine($"Cast int {a} to double {b}");
+
+double c = 9.8;
+int d = (int)c;
+Console.WriteLine($"Cast double {c} to int {d}");
+Console.ResetColor();
