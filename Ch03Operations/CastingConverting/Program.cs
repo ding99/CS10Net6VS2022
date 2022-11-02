54cea9b4c88202013073a1b3c46e897a01be541f
Wei Ding <ding_web@yahoo.co.jp>
2022-11-02 00:33:52 -0700

    lost info

--- Ch03Operations/CastingConverting/Program.cs
+++ Ch03Operations/CastingConverting/Program.cs
@@ -8,4 +8,18 @@ Console.WriteLine($"Cast int {a} to double {b}");
 double c = 9.8;
 int d = (int)c;
 Console.WriteLine($"Cast double {c} to int {d}");
+Console.ForegroundColor= ConsoleColor.Cyan;
+
+long e = 10;
+int f = (int)e;
+Console.WriteLine($"Cast long {e} to int {f}");
+
+e = long.MaxValue;
+f = (int)e;
+Console.WriteLine($"Cast long {e} to int {f}");
+
+e = 5_000_000_000;
+f = (int)e;
+Console.WriteLine($"Cast long {e} to int {f}");
+
 Console.ResetColor();
