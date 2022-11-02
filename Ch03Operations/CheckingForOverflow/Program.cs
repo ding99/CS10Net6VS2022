91c847f1aaaffaae8d85272f13c7468d7081fcdd
Wei Ding <ding_web@yahoo.co.jp>
2022-11-02 15:17:08 -0700

    catch checked

--- Ch03Operations/CheckingForOverflow/Program.cs
+++ Ch03Operations/CheckingForOverflow/Program.cs
@@ -1,15 +1,20 @@
 ﻿Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("- Check for overflow");
 
-checked {
-    int x = int.MaxValue - 1;
-    Console.WriteLine($"Initial value: {x}");
-    x++;
-    Console.WriteLine($"After incrementing: {x}");
-    x++;
-    Console.WriteLine($"After incrementing: {x}");
-    x++;
-    Console.WriteLine($"After incrementing: {x}");
+try {
+    checked {
+        int x = int.MaxValue - 1;
+        Console.WriteLine($"Initial value: {x}");
+        x++;
+        Console.WriteLine($"After incrementing: {x}");
+        x++;
+        Console.WriteLine($"After incrementing: {x}");
+        x++;
+        Console.WriteLine($"After incrementing: {x}");
+    }
+}
+catch (OverflowException) {
+    Console.WriteLine($"The code overflowed but I caught the exception.");
 }
 
 Console.ResetColor();
