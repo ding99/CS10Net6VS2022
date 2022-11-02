69a156a44a9d13cbc858860381a271cfd9b1d2c6
Wei Ding <ding_web@yahoo.co.jp>
2022-11-02 12:54:24 -0700

    warning

--- Ch03Operations/HandlingExceptions/Program.cs
+++ Ch03Operations/HandlingExceptions/Program.cs
@@ -2,12 +2,13 @@
 Console.WriteLine("- Handling exceptions");
 
 Console.WriteLine("Before parsing");
-string? input = "49";
-Console.WriteLine($"input is {input} with type of {input.GetType()}");
+string? input = Console.ReadLine();
+Console.WriteLine($"input is {input}");
 
 Console.WriteLine("What is your age?");
 try {
     int age = int.Parse(input);
+    Console.WriteLine($"You are {age} years old.");
 }
 catch {
 }
