5f805dcd1222bdb6a33646f629d9a67af97f910d
Wei Ding <ding_web@yahoo.co.jp>
2022-11-02 12:43:24 -0700

    exceptions

---
+++ Ch03Operations/HandlingExceptions/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("- Handling exceptions");
+
+Console.WriteLine("Before parsing");
+string? input = "49";
+Console.WriteLine($"input is {input} with type of {input.GetType()}");
+
+Console.WriteLine("What is your age?");
+try {
+    int age = int.Parse(input);
+}
+catch {
+}
+Console.WriteLine("After parsing");
+
+Console.ResetColor();
