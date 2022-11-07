695a612ef24ce460be241c07bd04baa96fe3f894
Wei Ding <ding_web@yahoo.co.jp>
2022-11-06 20:33:34 -0800

    trace lister

--- Ch04Functions/Instrumenting/Program.cs
+++ Ch04Functions/Instrumenting/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System.Diagnostics;
 
+Trace.Listeners.Add(new TextWriterTraceListener(
+    File.Create(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt"))));
+Trace.AutoFlush = true;
+
 Debug.WriteLine("Debug says, I am watching!");
 Trace.WriteLine("Trace says, I am watching!");
+
 Console.ReadLine();
