c85369c2545876c09db4412a625897245f18ea74
Wei Ding <ding_web@yahoo.co.jp>
2022-11-12 22:53:28 -0800

    call stack

---
+++ Ch04Functions/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,15 @@
+﻿using Packt;
+
+Console.WriteLine("In Main");
+
+Alpha();
+
+static void Alpha() {
+    Console.WriteLine("In Alpha");
+    Beta();
+}
+
+static void Beta() {
+    Console.WriteLine("In Beta");
+    Calculator.Gamma();
+}
