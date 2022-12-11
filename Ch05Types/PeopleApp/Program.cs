faba006e113d498caa27bc007bebd88df8a86060
Wei Ding <ding_web@yahoo.co.jp>
2022-12-10 21:22:12 -0800

    enum

--- Ch05Types/PeopleApp/Program.cs
+++ Ch05Types/PeopleApp/Program.cs
@@ -5,7 +5,8 @@ Console.ForegroundColor = ConsoleColor.Yellow;
 var bob = new Person
 {
     Name = "Bob Smith",
-    DateOfBirth = new DateTime (1966, 3, 7)
+    DateOfBirth = new DateTime (1966, 3, 7),
+    FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia
 };
 
 Console.WriteLine ("{0} was born on {1}",
@@ -20,5 +21,10 @@ Console.WriteLine ("{0} was born on {1:dd MMM yy}",
 Console.WriteLine ("{0} was born on {1:d MM yy}",
     arg0: bob.Name,
     arg1: bob.DateOfBirth);
+
+Console.WriteLine ("{0}'s favorite wonder is {1}. Its integer is {2}.",
+    bob.Name,
+    bob.FavoriteAncientWonder,
+    (int)bob.FavoriteAncientWonder);
 
 Console.ResetColor ();
