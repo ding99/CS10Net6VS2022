e61b0e94d21b6f6edfa5ea4269627da8bae10aa2
Wei Ding <ding_web@yahoo.co.jp>
2022-12-10 21:03:34 -0800

    date format

--- Ch05Types/PeopleApp/Program.cs
+++ Ch05Types/PeopleApp/Program.cs
@@ -1,4 +1,24 @@
 ﻿using Packt.Shared;
 
-Person bob = new();
-Console.WriteLine(bob.ToString());
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+var bob = new Person
+{
+    Name = "Bob Smith",
+    DateOfBirth = new DateTime (1966, 3, 7)
+};
+
+Console.WriteLine ("{0} was born on {1}",
+    arg0: bob.Name,
+    arg1: bob.DateOfBirth);
+Console.WriteLine("{0} was born on {1:dddd, d MMMM yyyy}",
+    arg0:bob.Name,
+    arg1:bob.DateOfBirth);
+Console.WriteLine ("{0} was born on {1:dd MMM yy}",
+    arg0: bob.Name,
+    arg1: bob.DateOfBirth);
+Console.WriteLine ("{0} was born on {1:d MM yy}",
+    arg0: bob.Name,
+    arg1: bob.DateOfBirth);
+
+Console.ResetColor ();
