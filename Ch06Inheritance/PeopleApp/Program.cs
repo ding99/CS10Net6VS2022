dca668b35538c0e2553e8b8d064490fb36bfe0e8
Wei Ding <ding_web@yahoo.co.jp>
2023-02-05 11:18:14 -0800

    try non-generic types

--- Ch06Inheritance/PeopleApp/Program.cs
+++ Ch06Inheritance/PeopleApp/Program.cs
@@ -30,6 +30,17 @@ harry.Poke();
 harry.Poke();
 harry.Poke();
 
+ForegroundColor = ConsoleColor.DarkYellow;
+System.Collections.Hashtable lookupObject = new();
+lookupObject.Add(key: 1, value: "Alpha");
+lookupObject.Add(key: 2, value: "Beta");
+lookupObject.Add(key: 3, value: "Gamma");
+lookupObject.Add(key: 4, value: "Delta");
+int key = 2;
+WriteLine(format: "Key {0} has value: {1}", arg0: key, arg1: lookupObject[key]);
+WriteLine(format: "Key {0} has value: {1}", arg0: harry, arg1: lookupObject[harry]);
+
+
 ResetColor();
 
 static void Harry_Shout(object? sender, EventArgs e)
