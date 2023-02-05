667cced923669487657498d9b4b421d9ca8b292c
Wei Ding <ding_web@yahoo.co.jp>
2023-02-05 00:34:52 -0800

    handle delegate

--- Ch06Inheritance/PeopleApp/Program.cs
+++ Ch06Inheritance/PeopleApp/Program.cs
@@ -21,8 +21,20 @@ WriteLine(format:"{0}'s first child is named \"{1}\".",
     arg0:harry.Name,
     arg1: harry.Children[0].Name);
 
-ForegroundColor = ConsoleColor.Cyan;
-
 WriteLine($"5! is {Person.Factorial(5)}");
 
+ForegroundColor = ConsoleColor.Cyan;
+harry.Shout = Harry_Shout;
+harry.Poke();
+harry.Poke();
+harry.Poke();
+harry.Poke();
+
 ResetColor();
+
+static void Harry_Shout(object? sender, EventArgs e)
+{
+    if(sender is null) return;
+    Person p = (Person)sender;
+    WriteLine($"{p.Name} is this angry: {p.AngerLevel}.");
+}
