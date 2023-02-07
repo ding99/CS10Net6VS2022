d9c8cd054a7d806063460b4943c5a3f4fbb90964
Wei Ding <ding_web@yahoo.co.jp>
2023-02-07 01:30:01 -0800

    make value type nullable

---
+++ Ch06Inheritance/NullHandling/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+ForegroundColor = ConsoleColor.Yellow;
+
+int thisCannotBeNull = 4;
+//thisCannotBeNull = null;
+int? thisCouldBeNull = null;
+WriteLine($"null: [{thisCouldBeNull}]");
+WriteLine($"null default: [{thisCouldBeNull.GetValueOrDefault()}]");
+thisCouldBeNull = 7;
+WriteLine($"7: [{thisCouldBeNull}]");
+WriteLine($"7 default: [{thisCouldBeNull.GetValueOrDefault()}]");
+
+
