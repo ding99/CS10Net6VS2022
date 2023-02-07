57cd7c06a47db3fabccf6dbaa51c3289d602a85d
Wei Ding <ding_web@yahoo.co.jp>
2023-02-07 02:04:40 -0800

    non-nullable variables

--- Ch06Inheritance/NullHandling/Program.cs
+++ Ch06Inheritance/NullHandling/Program.cs
@@ -11,4 +11,17 @@ thisCouldBeNull = 7;
 WriteLine($"7: [{thisCouldBeNull}]");
 WriteLine($"7 default: [{thisCouldBeNull.GetValueOrDefault()}]");
 
+ForegroundColor = ConsoleColor.Cyan;
+Address address = new();
+address.Building = null;
+//address.Street = null;
+address.City = "London";
+//address.Region = null;
 
+class Address
+{
+    public string? Building;
+    public string Street = string.Empty;
+    public string City = string.Empty;
+    public string Region = string.Empty;
+}
