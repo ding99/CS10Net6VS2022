30fa939b7d81d91194260c5e803675e0f95796b7
Wei Ding <ding_web@yahoo.co.jp>
2023-02-07 23:25:32 -0800

    hide members

--- Ch06Inheritance/PacktLibrary/Employee.cs
+++ Ch06Inheritance/PacktLibrary/Employee.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace Packt.Shared;
 
@@ -6,4 +6,9 @@ public class Employee : Person
 {
     public string? EmployeeCode { get; set; }
     public DateTime? HireDate { get; set; }
+
+    public new void WriteToConsole()
+    {
+        WriteLine(format:"{0} was born on {1:dd/MM/yy} and hired on {2:dd/MM/yy}", arg0:Name, arg1:DateOfBirth, arg2:HireDate);
+    }
 }
