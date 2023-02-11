670dde94b122d09c27e724d87edc8eb674d2f334
Wei Ding <ding_web@yahoo.co.jp>
2023-02-11 15:39:50 -0800

    publish

--- Ch07Packaging/DotNetEverywhere/Program.cs
+++ Ch07Packaging/DotNetEverywhere/Program.cs
@@ -1,5 +1,6 @@
 ﻿using static System.Console;
 
+ForegroundColor = ConsoleColor.Yellow;
 WriteLine("I can run everywhere!");
 WriteLine($"OS Version is {Environment.OSVersion}");
 if(OperatingSystem.IsMacOS())
@@ -8,3 +9,10 @@ else if(OperatingSystem.IsWindowsVersionAtLeast(major: 10))
     WriteLine("I am Windows 10 or 11.");
 else
     WriteLine("I am some other mysterious OS.");
+
+ResetColor();
+
+/***
+ dotnet publish -c Release -r win10-x64
+ dotnet publish -c Release -r linux-x64
+ ***/
