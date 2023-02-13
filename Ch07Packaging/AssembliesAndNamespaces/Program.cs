33f18dc04430cd1152524070276511266ef9b0d5
Wei Ding <ding_web@yahoo.co.jp>
2023-02-13 00:09:11 -0800

    port .net framework

--- Ch07Packaging/AssembliesAndNamespaces/Program.cs
+++ Ch07Packaging/AssembliesAndNamespaces/Program.cs
@@ -3,6 +3,8 @@ using System;
 using static System.Console;
 
 using Packt.Shared;
+using DialectSoftware.Collections;
+using DialectSoftware.Collections.Generics;
 
 ForegroundColor = ConsoleColor.Yellow;
 
@@ -29,5 +31,30 @@ Write("Enter a password: ");
 string? password = "secretsauce";
 WriteLine("Is {0} a valid password? {1}",
   arg0: password, arg1: password.IsValidPassWord());
+
+ForegroundColor = ConsoleColor.DarkYellow;
+
+Axis x = new("x", 0, 10, 1);
+Axis y = new("y", 0, 4, 1);
+Matrix<long> matrix = new(new[] { x, y });
+for(int i = 0; i < matrix.Axes[0].Points.Length; i++)
+{
+    matrix.Axes[0].Points[i].Label = "x" + i.ToString();
+}
+for(int i = 0; i < matrix.Axes[1].Points.Length; i++)
+{
+    matrix.Axes[1].Points[i].Label = "y" + i.ToString();
+}
+foreach(long[] c in matrix)
+{
+    matrix[c] = c[0] + c[1];
+}
+foreach(long[] c in matrix)
+{
+    WriteLine("{0},{1} ({2},{3}) = {4}",
+        matrix.Axes[0].Points[c[0]].Label,
+        matrix.Axes[1].Points[c[01]].Label,
+        c[0], c[1], matrix[c]);
+}
 
 ResetColor();
