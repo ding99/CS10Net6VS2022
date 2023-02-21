1a689ad98a265f082ad7b5d834afccecb790f936
Wei Ding <ding_web@yahoo.co.jp>
2023-02-20 23:49:27 -0800

    big numbers

---
+++ Ch8CommonTypes/WorkingWithNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+using static System.Console;
+
+ForegroundColor = ConsoleColor.Yellow;
+
+WriteLine("Working with large integers:");
+WriteLine("----------------------------");
+ulong big = ulong.MaxValue;
+WriteLine($"{big, 40:N0}");
+BigInteger bigger = BigInteger.Parse("123456789012345678901234567890");
+WriteLine($"{bigger, 40:N0}");
+
+ResetColor();
