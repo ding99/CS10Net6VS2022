4d3eec6704004821c73ff131036d05e289080d0b
Wei Ding <ding_web@yahoo.co.jp>
2023-02-22 09:53:16 -0800

    special time values

---
+++ Ch8CommonTypes/WorkingWithTime/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.WriteLine("Earliest date/time value is: {0}", arg0:DateTime.MinValue);
+Console.WriteLine("UNIX epoch date/time value is: {0}", arg0:DateTime.UnixEpoch);
+Console.WriteLine("Date/time value Now is: {0}", arg0:DateTime.Now);
+Console.WriteLine("Date/time value Today is: {0}", arg0:DateTime.Today);
+
+Console.ResetColor();
