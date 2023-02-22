3d3e673d1580a60136ea8172d413efb73ec87686
Wei Ding <ding_web@yahoo.co.jp>
2023-02-22 09:04:39 -0800

    split

--- Ch8CommonTypes/WorkingWithText/Program.cs
+++ Ch8CommonTypes/WorkingWithText/Program.cs
@@ -6,4 +6,12 @@ string city = "London";
 WriteLine($"{city} is {city.Length} characters long.");
 WriteLine($"First char is {city[0]} and third is {city[2]}.");
 
+string cities = "Paris,Tehran,CHennai,Sydney,New York,Medellin";
+string[] citiesArray = cities.Split(',');
+WriteLine($"There are {citiesArray.Length} items in the array.");
+foreach(string item in citiesArray)
+{
+    WriteLine(item);
+}
+
 ResetColor();
