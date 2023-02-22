5155e8d8a7f943e85a68014df1ff176f40a88f73
Wei Ding <ding_web@yahoo.co.jp>
2023-02-22 10:20:25 -0800

    christmas

--- Ch8CommonTypes/WorkingWithTime/Program.cs
+++ Ch8CommonTypes/WorkingWithTime/Program.cs
@@ -5,4 +5,13 @@ Console.WriteLine("UNIX epoch date/time value is: {0}", arg0:DateTime.UnixEpoch)
 Console.WriteLine("Date/time value Now is: {0}", arg0:DateTime.Now);
 Console.WriteLine("Date/time value Today is: {0}", arg0:DateTime.Today);
 
+Console.WriteLine();
+
+DateTime christmas = new(year:2023, month:12, day:25);
+Console.WriteLine("Christmas: {0}", arg0:christmas);
+Console.WriteLine("Christmas: {0:dddd, dd MMMM yyyy}", arg0:christmas);
+Console.WriteLine("Christmas is in month {0} of the year.", arg0: christmas.Month);
+Console.WriteLine("Christmas is day {0} of the year.", arg0:christmas.DayOfYear);
+Console.WriteLine("Christmas {0} is on a {1}.", arg0:christmas.Year, arg1:christmas.DayOfWeek);
+
 Console.ResetColor();
