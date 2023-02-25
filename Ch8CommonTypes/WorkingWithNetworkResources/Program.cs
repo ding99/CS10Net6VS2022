65a0d2b0ae56e39d85c34d1935c9e7bdf7f81684
Wei Ding <ding_web@yahoo.co.jp>
2023-02-25 14:43:17 -0800

    uri

---
+++ Ch8CommonTypes/WorkingWithNetworkResources/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using static System.Console;
+
+ForegroundColor = ConsoleColor.Yellow;
+
+Write("Enter a valid web address:");
+string? url = ReadLine();
+if(string.IsNullOrEmpty(url))
+{
+    url = "https://stackoverflow.com/search?q=securestring";
+}
+Uri uri = new(url);
+WriteLine($"URL: {url}");
+WriteLine($"Scheme: {uri.Scheme}");
+WriteLine($"Port: {uri.Port}");
+WriteLine($"Host: {uri.Host}");
+WriteLine($"Path: {uri.AbsolutePath}");
+WriteLine($"Query: {uri.Query}");
+
+IPHostEntry entry = Dns.GetHostEntry(uri.Host);
+WriteLine($"{entry.HostName} has the following IP addresses:");
+foreach(IPAddress address in entry.AddressList)
+    WriteLine($" {address} ({address.AddressFamily})");
+
+ResetColor();
