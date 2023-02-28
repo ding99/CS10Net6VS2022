7de7e02bb23a426419ca7607cee7644223b985fa
Wei Ding <ding_web@yahoo.co.jp>
2023-02-28 00:35:32 -0800

    file system

---
+++ Ch09Files/FileSystems/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
+ForegroundColor = ConsoleColor.Yellow;
+
+OutputFileSystemInfo ();
+
+ResetColor ();
+
+
+static void OutputFileSystemInfo ()
+{
+    WriteLine("{0,-33} {1}", arg0: "Path.PathSeparator", arg1: PathSeparator);
+    WriteLine("{0,-33} {1}", arg0: "Path.DirectorySeparatorChar", arg1: DirectorySeparatorChar);
+    WriteLine("{0,-33} {1}", arg0: "Directory.GetCurrentDirectory()", arg1: GetCurrentDirectory());
+    WriteLine("{0,-33} {1}", arg0: "Environment.CurrentDirectory", arg1: CurrentDirectory);
+    WriteLine("{0,-33} {1}", arg0: "Environment.SystemDirectory)", arg1: SystemDirectory);
+    WriteLine("{0,-33} {1}", arg0: "Path.GetTempPath()", arg1: GetTempPath());
+
+    WriteLine("GetFolderPath(SpecialFolder");
+    WriteLine ("{0,-33} {1}", arg0: ".System", arg1: GetFolderPath(SpecialFolder.System));
+    WriteLine ("{0,-33} {1}", arg0: ".ApplicationData", arg1: GetFolderPath (SpecialFolder.ApplicationData));
+    WriteLine ("{0,-33} {1}", arg0: ".MyDocuments", arg1: GetFolderPath (SpecialFolder.MyDocuments));
+    WriteLine ("{0,-33} {1}", arg0: ".Personal", arg1: GetFolderPath (SpecialFolder.Personal));
+}
