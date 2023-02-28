3543c81949df8d28811d7101c97b1a4ed2ea0e00
Wei Ding <ding_web@yahoo.co.jp>
2023-02-27 21:38:02 -0800

    version

---
+++ Ch8CommonTypes/WorkingWithReflection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+using static System.Console;
+
+ForegroundColor = ConsoleColor.Yellow;
+
+WriteLine("Assembly metadata:");
+
+Assembly? asm = Assembly.GetEntryAssembly();
+if(asm is null)
+{
+    WriteLine("Failed to get entry assembly.");
+    return;
+}
+
+WriteLine($" Full name: {asm.FullName}");
+WriteLine($" Location: {asm.Location}");
+IEnumerable<Attribute> attrs = asm.GetCustomAttributes();
+WriteLine($" Assembly-level attributes:");
+foreach(Attribute a in attrs)
+    WriteLine($" {a.GetType()}");
+
+ForegroundColor = ConsoleColor.Cyan;
+
+AssemblyInformationalVersionAttribute? ver = asm.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+WriteLine($" Version: {ver?.InformationalVersion}");
+
+AssemblyCompanyAttribute? company = asm.GetCustomAttribute<AssemblyCompanyAttribute>();
+WriteLine($" Company: {company?.Company}");
+
+ResetColor();
