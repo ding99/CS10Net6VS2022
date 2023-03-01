e950f4490f265e5774a74c3c15aed3fc1e2407a6
Wei Ding <ding_web@yahoo.co.jp>
2023-03-01 00:49:52 -0800

    file info

--- Ch09Files/FileSystems/Program.cs
+++ Ch09Files/FileSystems/Program.cs
@@ -90,4 +90,10 @@ static void WorkWithFiles ()
     WriteLine ($"File Extension: {GetExtension (textFile)}");
     WriteLine ($"Random File Name: {GetRandomFileName ()}");
     WriteLine ($"Temporary File Name: {GetTempFileName()}");
+
+    FileInfo info = new FileInfo (backupFile);
+    WriteLine ($"{backupFile}:");
+    WriteLine ($"Contains {info.Length} bytes");
+    WriteLine ($"Last accessed {info.LastAccessTime}");
+    WriteLine ($"Has readonly set to {info.IsReadOnly}");
 }
