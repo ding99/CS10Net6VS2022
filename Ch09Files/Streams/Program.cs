8dcc9c72f60c28ba563157673c82d616e412c32b
Wei Ding <ding_web@yahoo.co.jp>
2023-03-01 01:47:54 -0800

    stream writer

---
+++ Ch09Files/Streams/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml;
+
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+ForegroundColor = ConsoleColor.Yellow; WorkWithText ();
+
+
+
+
+ResetColor ();
+
+static void WorkWithText ()
+{
+    string textFile = Combine (CurrentDirectory, "streams.txt");
+    StreamWriter text = File.CreateText (textFile);
+    foreach(string item in Viper.Callsigns)
+        text.WriteLine (item);
+    text.Close ();
+    WriteLine ("{0} contains {1:N0} bytes.", arg0:textFile, arg1:new FileInfo(textFile).Length);
+    WriteLine (File.ReadAllText (textFile));
+}
+
+static class Viper
+{
+    public static string[] Callsigns = new[]
+    {
+        "Husker",
+        "Starbuck",
+        "Apollo",
+        "Boomer",
+        "Bulldog",
+        "Athena",
+        "Helo",
+        "Racetrack"
+    };
+}
