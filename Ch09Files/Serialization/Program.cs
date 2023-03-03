1aed3579ae4e767321c905f65d262c27e266f755
Wei Ding <ding_web@yahoo.co.jp>
2023-03-03 00:06:55 -0800

    deserialization

--- Ch09Files/Serialization/Program.cs
+++ Ch09Files/Serialization/Program.cs
@@ -48,4 +48,14 @@ WriteLine("Written {0:N0} bytes of XML to {1}", arg0:new FileInfo(path).Length, 
 WriteLine();
 WriteLine(File.ReadAllText(path));
 
+ForegroundColor = ConsoleColor.Cyan;
+
+using FileStream xmlLoad = File.Open(path, FileMode.Open);
+List<Person>? loadedPeople = xs.Deserialize(xmlLoad) as List<Person>;
+if(loadedPeople is not null)
+    foreach(Person p in loadedPeople)
+    {
+        WriteLine("{0} has {1} children.", p.LastName, p.Children?.Count??0);
+    }
+
 ResetColor ();
