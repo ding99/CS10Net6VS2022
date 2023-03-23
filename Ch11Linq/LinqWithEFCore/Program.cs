bb037876ba0646a83ef376f50dcfd1287afce639
Wei Ding <ding_web@yahoo.co.jp>
2023-03-22 23:44:35 -0700

    project sequences

--- Ch11Linq/LinqWithEFCore/Program.cs
+++ Ch11Linq/LinqWithEFCore/Program.cs
@@ -14,9 +14,23 @@ static void FilterAndSort ()
     DbSet<Product> allProducts = db.Products!;
     IQueryable<Product> filteredProducts = allProducts.Where (p => p.UnitPrice < 10M);
     IOrderedQueryable<Product> sortedAndFilteredProducts = filteredProducts.OrderByDescending(p => p.UnitPrice);
+
     Console.WriteLine("Products that cost less than $10:");
     foreach(Product p in sortedAndFilteredProducts)
         Console.WriteLine("{0}: {1} costs {2:$#.##0.00}", p.ProductId, p.ProductName, p.UnitPrice);
     Console.WriteLine();
+
+    var projectedProducts = sortedAndFilteredProducts.Select (p => new
+    {
+        p.ProductId,
+        p.ProductName,
+        p.UnitPrice
+    });
+
+    Console.WriteLine ("Products that cost less than $10:");
+    foreach (var p in projectedProducts)
+        Console.WriteLine ("{0}: {1} costs {2:$#.##0.00}", p.ProductId, p.ProductName, p.UnitPrice);
+    Console.WriteLine ();
+
 }
 
