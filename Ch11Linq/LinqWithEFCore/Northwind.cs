1e1e3ef1a83e2ce2418666193950d3623a0dd623
Wei Ding <ding_web@yahoo.co.jp>
2023-03-22 21:43:15 -0700

    build EF Core model

---
+++ Ch11Linq/LinqWithEFCore/Northwind.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore; // DbContext, DbSet<T>
+
+namespace Packt.Shared;
+
+public class Northwind : DbContext
+{
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+    protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+    {
+        string path = Path.Combine (Environment.CurrentDirectory, "Northwind.db");
+        optionsBuilder.UseSqlite($"Filename={path}");
+
+        //string connection = "Data Source=.'" +
+        //"Initial Catalog=Northwind;" +
+        //"Integrated Security=true;" +
+        //"MultipleActiveResultSets=true;";
+        //optionsBuilder.UseSqlite(connection);
+    }
+
+    protected override void OnModelCreating (ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product> ()
+            .Property (product => product.UnitPrice)
+            .HasConversion<double> ();
+    }
+}
---
+++ Ch11Linq/LinqWithEFCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Packt.Shared;
+
+using static System.Console;
+
+ForegroundColor = ConsoleColor.Yellow;
+
+
+ResetColor ();
+
+static void FilterAndSort ()
+{
+    using Northwind db = new ();
+    DbSet<Product> allProducts = db.Products;
+}
+
