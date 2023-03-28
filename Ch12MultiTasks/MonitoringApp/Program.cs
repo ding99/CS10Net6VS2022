de59be7a3c4100d73be1efdb0be0a4ff128abea5
Wei Ding <ding_web@yahoo.co.jp>
2023-03-28 01:25:24 -0700

    compare string joining

--- Ch12MultiTasks/MonitoringApp/Program.cs
+++ Ch12MultiTasks/MonitoringApp/Program.cs
@@ -1,14 +1,31 @@
-﻿using Packt.Shared;
+﻿using System.Text;
+using Packt.Shared;
 
 using static System.Console;
 
 ForegroundColor = ConsoleColor.Yellow;
 
-WriteLine("Processing. Please wait...");
-Recorder.Start();
+//WriteLine("Processing. Please wait...");
+//Recorder.Start();
+//int[] largeArrayOfInts = Enumerable.Range(start: 1, count: 10_000).ToArray();
+//Thread.Sleep(new Random().Next(5, 10) * 1000);
+//Recorder.Stop();
 
-int[] largeArrayOfInts = Enumerable.Range(start:1, count:10_000).ToArray();
-Thread.Sleep(new Random().Next(5,10)*1000);
+int[] numbers = Enumerable.Range(start:1,count:50_000).ToArray();
+WriteLine("Using string with +");
+Recorder.Start();
+string s = string.Empty;
+for (int i = 0; i < numbers.Length; i++)
+    s += numbers[i] + ",";
+Recorder.Stop();
+
+ForegroundColor = ConsoleColor.DarkYellow;
+
+WriteLine("Using StringBuilder");
+Recorder.Start();
+StringBuilder builder = new();
+for (int i = 0; i < numbers.Length; i++)
+    builder.Append(numbers[i]).AppendLine(",");
 Recorder.Stop();
 
 ResetColor();
--- Ch12MultiTasks/MonitoringLib/Recorder.cs
+++ Ch12MultiTasks/MonitoringLib/Recorder.cs
@@ -7,7 +7,7 @@ namespace Packt.Shared;
 
 public static class Recorder
 {
-    private static Stopwatch timer = new();
+    private static readonly Stopwatch timer = new();
     private static long bytesPhysicalBefore = 0;
     private static long bytesVirtualBefore = 0;
 
@@ -28,7 +28,7 @@ public static class Recorder
     {
         timer.Stop();
         long bytesPhysicalAfter = GetCurrentProcess().WorkingSet64;
-        long bytesVirtualAfter = GetCurrentProcess().PeakVirtualMemorySize64;
+        long bytesVirtualAfter = GetCurrentProcess().VirtualMemorySize64;
         WriteLine("{0:N0} physical bytes used.", bytesPhysicalAfter - bytesPhysicalBefore);
         WriteLine("{0:N0} virtual bytes used.", bytesVirtualAfter - bytesVirtualBefore);
         WriteLine("{0} time span ellapsed.", timer.Elapsed);
