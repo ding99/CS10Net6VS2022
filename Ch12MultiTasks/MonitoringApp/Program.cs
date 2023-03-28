6ccb8097de0ddb5a5f8c95b2080ea3d2c48cc896
Wei Ding <ding_web@yahoo.co.jp>
2023-03-28 01:10:18 -0700

    record momory and timings

--- Ch12MultiTasks/MonitoringApp/Program.cs
+++ Ch12MultiTasks/MonitoringApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Packt.Shared;
+
+using static System.Console;
+
+ForegroundColor = ConsoleColor.Yellow;
+
+WriteLine("Processing. Please wait...");
+Recorder.Start();
+
+int[] largeArrayOfInts = Enumerable.Range(start:1, count:10_000).ToArray();
+Thread.Sleep(new Random().Next(5,10)*1000);
+Recorder.Stop();
+
+ResetColor();
