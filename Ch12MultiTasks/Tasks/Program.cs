80fe7aff7704f7461531296f8489a06d9e88e79f
Wei Ding <ding_web@yahoo.co.jp>
2023-03-28 23:08:22 -0700

    using Task

--- Ch12MultiTasks/Tasks/Program.cs
+++ Ch12MultiTasks/Tasks/Program.cs
@@ -2,7 +2,7 @@
 
 using static System.Console;
 
-ForegroundColor = ConsoleColor.Yellow;
+ForegroundColor = ConsoleColor.Cyan;
 
 Stopwatch timer = Stopwatch.StartNew();
 WriteLine("Running methods synchronously on one thread.");
@@ -10,6 +10,15 @@ MethodA();
 MethodB();
 MethodC();
 WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+
+ForegroundColor = ConsoleColor.Yellow;
+Stopwatch timer2 = Stopwatch.StartNew();
+WriteLine("Running methods asynchronously on multiple threads.");
+Task taskA = new(MethodA);
+taskA.Start();
+Task taskB = Task.Factory.StartNew(MethodB);
+Task taskC = Task.Run(MethodC);
+WriteLine($"{timer2.ElapsedMilliseconds:#,##0}ms elapsed.");
 
 ResetColor();
 
