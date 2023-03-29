e126f50d25054d0ce506b39c7f3014f8188b426d
Wei Ding <ding_web@yahoo.co.jp>
2023-03-29 00:33:34 -0700

    nested tasks

---
+++ Ch12MultiTasks/NestedAndChildTasks/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+ForegroundColor = ConsoleColor.Yellow;
+
+Task outerTask = Task.Factory.StartNew(OuterMethod);
+outerTask.Wait();
+WriteLine("Console appl is stopping.");
+
+ResetColor();
+
+static void OuterMethod()
+{
+    WriteLine("Outer method starting...");
+    Task innerTask = Task.Factory.StartNew(InnerMethod, TaskCreationOptions.AttachedToParent);
+    WriteLine("Outer method finished.");
+}
+
+static void InnerMethod()
+{
+    WriteLine("Inner method starting...");
+    Thread.Sleep(2000);
+    WriteLine("Inner method finished.");
+}
